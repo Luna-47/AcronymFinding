b4737eecab9686b6a3f0fae42307e6874aafb753
Luna Manandhar <luna.manandhar4@gmail.com>
2022-01-27 17:01:04 +0545

    Full-Form added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,23 @@
-var list={
-  'www':['World Wide Web','weeweewe'],
-  'abc':['Attanasoff Berry Computer','aboydabc']
+const list={
+  'www':['World Wide Web'],
+  'abc':['Attanasoff-Berry Computer','American Broadcasting Company','Automatic Brightness Control','Active Body Control'],
+  'ram':['Random Access Memory'],
+  'rom':['Read Only Memomy'],
+  'bas':['Business Activity Statement','Bachelor of Applied Studies','Behavorial Analysis Science'],
+  'bit':['binary digit','Bachelor of Information Technology','Baggage Identification Tag'],
+  'url':['Uniform Resource Locator'],
+  'tcp':['Transmission Control Protocol'],
+  'ups':['Uninterruptible Power Supply']
 }
 
 function showresult(){
-  var text=document.getElementById('search');
+  var search=document.getElementById('search');
   console.log(search.value)
   var result=document.getElementById('result');
+  result.innerHTML=""
 
   for (var i=0; i<list[search.value.toLowerCase()].length; i++){
-    let li=document.createElement("li");
+    const li=document.createElement("li");
     li.textContent=list[search.value][i];
     result.appendChild(li);
   }
